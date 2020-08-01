fb827519b702051021c5c13e0bfbf74ca9c70e9c
razamuhin <razamuhin@outlook.com>
2020-07-31 22:41:40 -0400

    o Replaced for loop with while o Swapped status tracking ints for bools

--- myshell.c
+++ myshell.c
@@ -20,9 +20,11 @@ char *args[MAX_LENGTH];
 char buffer[100];
 
 // parse_input - takes user input and breaks it into a matrix of arguments
-// *input - pointer to user input
-int parse_input(char *input){
-	int bgcheck = 0, i = 0;
+// Reference:
+// https://brennan.io/2015/01/16/write-a-shell-in-c/ (see main)
+bool parse_input(char *input){
+	bool bgcheck = false;
+	int i = 0;
 	input[strlen(input)-1] = '\0';
 	args[0] = strtok(input," ");
 	while(args[i] != NULL){
@@ -36,71 +38,62 @@ int parse_input(char *input){
 		args[i+1] = strtok(NULL," ");
 		++i;
 	}
-	if(args[0] != NULL && strcmp(args[i-1],"&")==0){ bgcheck = 1; }
+	if(args[0] != NULL && strcmp(args[i-1],"&")==0){ bgcheck = true; }
 	return bgcheck;
 }
 
 // execute shell command
-// reference: https://stackoverflow.com/questions/8319484/regarding-background-processes-using-fork-and-child-processes-in-my-dummy-shel
-int exec_cmd(int bgcheck){
-	int i = 0, err_chk = 1, status;
+// Used this reference:
+// https://stackoverflow.com/questions/8319484/regarding-background-processes-using-fork-and-child-processes-in-my-dummy-shel
+int exec_cmd(bool bgcheck){
+	int i = 0, status;
+	bool err_chk = true;
 	pid_t pid;
 	pid = fork();
 	if (pid==0) {
-		//while
-		for(i=0;i<4;i++){
+		// Compare first argument with list of available functions
+		while(commands[i]!=NULL){
 			if (strcmp(args[0], commands[i]) == 0 ) {
 				printf("Starting child process %s with pid %d\n",args[0],getpid());
-				err_chk = 0;
-				return (*cmd[i])(args);
+				err_chk = false;
+				return (*cmd[i])(args);									// Execute function with arguments
 			}
+			i++;
 		}
 		exit(0);
-		if (err_chk == 1) {
+		if (err_chk) {
 			fprintf(stderr,"Command not recognized!\n");
 			exit(0);
 		}
 	} else {
-		if(bgcheck==0){ waitpid(pid,&status,0); }
+		if(!bgcheck){ waitpid(pid,&status,0); }
 	}
 	return 0;
 }
 
 int main(void){
 	// References:
-	// https://stackoverflow.com/questions/1247989/how-do-you-allow-spaces-to-be-entered-using-scanf
-	// https://brennan.io/2015/01/16/write-a-shell-in-c/
-	// Allocate memory for user arguments, and exit gracefully if no memory is available.
+	// https://stackoverflow.com/questions/1247989/how-do-you-allow-spaces-to-be-entered-using-scanf - for fgets examples
+	// https://brennan.io/2015/01/16/write-a-shell-in-c/ - used this article's model for storing command line arguments and calling builtin shell functions
 	char input[MAX_LENGTH];
+	bool bgcheck = false, cont = true;							// While loop exit condition
+	printf("Shell pid is %d\n",getpid());						// Print the process id for this command loop
 
-	//don't need
-	if(input == NULL){
-		fprintf(stderr,"Out of memory - please download additional RAM\n");
-		return 1;
-	}
-
-	int bgcheck;
-	bool cont = true;	// # CHANGE TO BOOL
-
-	// use getpid() to get and print the process id for this command loop
-	printf("Shell pid is %d\n",getpid());
 	// prompt user for next command
-
 	while(cont){
 		// shell prompt
-		printf("wash> ");
+		printf("msh > ");
 		fflush(stdout);
 		fgets(input, MAX_LENGTH, stdin);
 
 		// parse command
 		bgcheck = parse_input(input);
-		if (args[0] == NULL) {												// if no text recieved, print error
+		if (args[0] == NULL) {												// If no text recieved, print error
 			fprintf(stderr,"No command received\n");
-		} else if (strcmp(args[0], "halt") == 0) {		// if command is halt, exit shell
+		} else if (strcmp(args[0], "halt") == 0) {		// If command is halt, exit shell
 			cont = false;
 		} else {
-			// fork and execute new process
-			exec_cmd(bgcheck);
+			exec_cmd(bgcheck);													// Fork and execute new process
 		}
 	}
 
--- shellfuncts.c
+++ shellfuncts.c
@@ -19,18 +19,16 @@ int create(char **arguments){
 	} else {
 		FILE *newfile;
 		newfile = fopen(arguments[1],"w");
-		//check for leading whitespace
+		// Check for leading whitespace
 		fclose(newfile);
 	}
 	exit(0);
 }
 
 // update - appends lines of text to a file
-// filename - the name of the target file
-// numlines - number of lines to add
-// text - text in each line
 int update(char **arguments){
 	int numlines, n;
+	// Check whether file exists
 	if(fopen(arguments[1],"r")==NULL){
 		fprintf(stderr,"[ERROR]: File '%s' not found!\n",arguments[1]);
 	} else {
@@ -49,11 +47,9 @@ int update(char **arguments){
 }
 
 // list - displays contents of a file
-//		Parameters:
-// 			arguments - the name of the target file
-//		Maximum file size: 1MB
 int list(char **arguments){
 	char filebuffer[100];
+	// Check whether file exists
 	if(fopen(arguments[1],"r")==NULL){
 		fprintf(stderr, "File '%s' not found!\n",arguments[1]);
 	} else {
@@ -63,7 +59,7 @@ int list(char **arguments){
 	exit(0);
 }
 
-// dir - uses the Linux ls system builtin to list files in the current directory
+// dir - uses the Linux ls binary to list files in the current directory
 int dir(){
 	execl("/bin/ls","ls",NULL);
 	exit(0);
